3b47cc5951b29308c97b40a4d155fafd44548bfe
Filip Liwiński <filipliwinski@users.noreply.github.com>
2023-09-27 23:40:01 +0200

    Improved logging

--- src/photo_cleaner/photo_cleaner.py
+++ src/photo_cleaner/photo_cleaner.py
@@ -8,49 +8,76 @@ import logging
 import os
 import sys
 
+
 from version import __version__
 
 SCRIPT_CODE_NAME = "photo_cleaner"
-SCRIPT_DESCRIPTION = "This script removes files stored in hidden folders."
+SCRIPT_DESCRIPTION = "This script removes hidden files and folders."
 SCRIPT_NAME = f"Photo Cleaner v.{__version__}"
 
-def _cleanup_photos(photos_location, dry_run):
-    """"""
+START_TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
+LOG_FILE_NAME = f"{SCRIPT_CODE_NAME}_{START_TIMESTAMP}.log"
 
-    directories = os.walk(photos_location)
+logger = logging.getLogger(SCRIPT_CODE_NAME)
+logger.setLevel(logging.DEBUG)
 
-    for directory in directories:
-        # Get the last part of the path
-        target_folder = directory[0].split("\\")[-1]
-        print(directory[0])
+log_handler = logging.FileHandler(LOG_FILE_NAME)
+log_handler.setLevel(logging.DEBUG)
+logger.addHandler(log_handler)
 
-        # Identify hidden folders
-        if target_folder[0] == ".":
-            print(f"{target_folder} [{directory[0]}]")
 
-            # Get files in directory
-            files = directory[2]
-            print(files)
+def _delete_items(items_to_remove, dry_run):
+    """Deletes items from disk."""
 
-            for file in files:
-                if file.lower().endswith(".jpg") or file.lower().endswith(".jpeg"):
-                    print(file)
+    for item in items_to_remove:
+        logger.info("Deleted %s", item)
+
+
+def _should_be_removed(full_path, item_age_in_hours):
+    """Checks if an item should be removed, based on its last modification date."""
+
+    return True
+
+
+def _find_items_to_remove(photos_location, max_age_in_hours):
+    """Finds items to remove."""
+
+    items_to_remove = list()
+
+    for root, directories, files in os.walk(photos_location):
+
+        logger.debug(root)
+
+        for directory in directories:
+            if directory[0] == ".":
+                full_path = os.path.join(root, directory)
+                if _should_be_removed(full_path, max_age_in_hours):
+                    logger.debug("To delete: %s", full_path)
+                    items_to_remove.append(full_path)
+
+        for file in files:
+            if file[0] == ".":
+                full_path = os.path.join(root, file)
+                if items_to_remove.count(root) == 0:
+                    if _should_be_removed(full_path, max_age_in_hours):
+                        logger.debug("To delete: %s", full_path)
+                        items_to_remove.append(full_path)
+
+    return items_to_remove
+
 
 def main():
     """Validates arguments and executes the script."""
 
-    current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename=f"{SCRIPT_CODE_NAME}_{current_timestamp}.log",
-        filemode="w")
-
-    logging.info(SCRIPT_NAME)
+    logger.info(SCRIPT_NAME)
     print(SCRIPT_NAME)
 
-    parser = argparse.ArgumentParser(prog=SCRIPT_NAME, description=SCRIPT_DESCRIPTION)
-    parser.add_argument("-p", "--photos_location", help="The location of Synology Photos folder.")
-    parser.add_argument("-d", "--dry_run", help="Enables dry run.", action="store_true")
+    parser = argparse.ArgumentParser(
+        prog=SCRIPT_NAME, description=SCRIPT_DESCRIPTION)
+    parser.add_argument("-p", "--photos_location",
+                        help="The location of Synology Photos folder.")
+    parser.add_argument("-d", "--dry_run",
+                        help="Enables dry run.", action="store_true")
 
     args = parser.parse_args()
     photos_location = args.photos_location
@@ -60,7 +87,9 @@ def main():
     if not os.path.isdir(photos_location):
         sys.exit(f"{photos_location} is not a valid directory.")
 
-    _cleanup_photos(photos_location, dry_run)
+    items_to_remove = _find_items_to_remove(photos_location, 1)
+    _delete_items(items_to_remove, dry_run)
+
 
 if __name__ == "__main__":
     main()
